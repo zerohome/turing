683c63c35c2da1a1c2b0fc162d6256c48fd8fbcd
Home <home@email.com>
2022-06-12 22:30:47 +0800

    spring-boot-auto-analyze

---
+++ spring-boot-auto-analyze/src/main/java/com/mini/spring/boot/auto/analyze/SpringBootAutoAnalyzeApp.java
@@ -0,0 +1,12 @@
+package com.mini.spring.boot.auto.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoAnalyzeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoAnalyzeApp.class, args);
+    }
+}
---
+++ spring-boot-auto-analyze/src/main/java/com/mini/spring/boot/auto/analyze/bo/User.java
@@ -0,0 +1,62 @@
+package com.mini.spring.boot.auto.analyze.bo;
+
+public class User {
+
+    private Integer userId;
+    private String userName;
+    private String password;
+    private String email;
+    private String birthDate;
+
+    public User(Integer userId, String userName, String password, String email, String birthDate) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public User() {
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+}
