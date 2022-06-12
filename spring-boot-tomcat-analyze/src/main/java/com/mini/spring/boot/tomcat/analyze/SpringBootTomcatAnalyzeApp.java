bb8f378bc77fa1cd54cc34117e77607545f433c9
Home <home@email.com>
2022-06-12 18:04:07 +0800

    spring-boot-tomcat-analyze

---
+++ spring-boot-tomcat-analyze/src/main/java/com/mini/spring/boot/tomcat/analyze/SpringBootTomcatAnalyzeApp.java
@@ -0,0 +1,12 @@
+package com.mini.spring.boot.tomcat.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTomcatAnalyzeApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomcatAnalyzeApp.class, args);
+    }
+}
