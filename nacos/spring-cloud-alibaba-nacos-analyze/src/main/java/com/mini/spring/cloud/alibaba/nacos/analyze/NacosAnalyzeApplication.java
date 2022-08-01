fef650bc8efc35e22fc943ee06c44a0c9fce4ca2
Home <home@email.com>
2022-08-01 23:23:15 +0800

    spring-cloud-alibaba-component nacos-analyze nacos源码解析

---
+++ nacos/spring-cloud-alibaba-nacos-analyze/src/main/java/com/mini/spring/cloud/alibaba/nacos/analyze/NacosAnalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.mini.spring.cloud.alibaba.nacos.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosAnalyzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosAnalyzeApplication.class, args);
+    }
+
+}
